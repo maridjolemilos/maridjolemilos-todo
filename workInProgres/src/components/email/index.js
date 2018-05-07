8d9b17a1521c7118f44448b91f98b3bc0094fcfa
marijapesic <marija_cka@yahoo.com>
2018-05-07 09:32:54 +0200

    makingsmallchangings

--- workInProgres/src/components/email/index.js
+++ workInProgres/src/components/email/index.js
@@ -13,4 +13,5 @@ render() {
       }
 }
 
+
 export default Email;
--- workInProgres/src/components/password/index.js
+++ workInProgres/src/components/password/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './index.css';
 
-
 const Password = (props) => {
     return (
        <div>  <input type="password" placeholder="Enter Password" name="psw" pattern=".{8,}"/> </div>
