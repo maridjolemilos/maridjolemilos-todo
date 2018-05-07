dfd5922d45fe9b912cf74ca64709c6cd3788f79e
mixyznik <mixy80@gmail.com>
2018-05-07 19:37:18 +0200

    changed arhitecture

--- workInProgres/src/components/signed/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Modal from 'react-modal';
-import fetch from 'cross-fetch'
-import './style.css';
-import firebase from '../../firebase/firebase';
-
--- workInProgres/src/containers/footer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = (props) => {
-    return (
-        <div>FOOTER</div>
-    )
-};
-
-export default Footer;
